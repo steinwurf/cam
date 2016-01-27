65bb4fb245f7276a26a0e20e94b3c06fc9d5fa30
Morten V. Pedersen <morten@mortenvp.com>
2016-01-27 01:54:45 +0100

    Cleaning up and adding aggregation strategy

--- examples/tcp_client/tcp_client.cpp
+++ examples/tcp_client/tcp_client.cpp
@@ -12,7 +12,9 @@
 #include <boost/asio.hpp>
 
 #include <sak/convert_endian.hpp>
+
 #include <n4lu/to_annex_b_nalu.hpp>
+#include <n4lu/to_annex_b_nalus.hpp>
 
 namespace ba = boost::asio;
 
@@ -34,7 +36,7 @@ void read_from_socket(ba::ip::tcp::socket& socket,
     ba::read(socket, ba::buffer(data, size));
 }
 
-int main(int argc, char* argv[])
+int run_client(int argc, char* argv[])
 {
     (void) argc;
     (void) argv;
@@ -96,4 +98,79 @@ int main(int argc, char* argv[])
     }
 
     return 0;
+}
+
+int run_client_v2(int argc, char* argv[])
+{
+    (void) argc;
+    (void) argv;
+
+    try
+    {
+        ba::io_service io_service;
+
+        ba::ip::tcp::socket s(io_service);
+
+        auto endpoint = ba::ip::tcp::endpoint(
+            ba::ip::address_v4::from_string("127.0.0.1"), 54321);
+
+
+        // For the captured data
+        std::vector<uint8_t> buffer;
+
+        // The time stamp of the previous captured frame (needed to
+        // calculate sample time)
+        uint64_t previous_timestamp = 0;
+
+        s.connect(endpoint);
+
+        uint32_t width = read_from_socket<uint32_t>(s);
+        uint32_t height = read_from_socket<uint32_t>(s);
+
+        std::cout << "w = " << width << " h = " << height << std::endl;
+
+        // Counts the number of capture segments
+        uint32_t frames = 0;
+        while(1)
+        {
+
+            uint64_t timestamp = read_from_socket<uint64_t>(s);
+
+            // The sample time (timestamp delta)
+            uint64_t sample_time = timestamp - previous_timestamp;
+
+            uint32_t size = read_from_socket<uint32_t>(s);
+
+            buffer.resize(size);
+
+            read_from_socket(s, buffer.data(), size);
+
+            std::cout << frames << ": size = " << size << " "
+                      << "timestamp = " << timestamp << " "
+                      << "sampletime = " << sample_time << std::endl;
+
+            auto nalus = n4lu::to_annex_b_nalus(buffer.data(), buffer.size());
+
+            for(const auto& nalu : nalus)
+            {
+                std::cout << "  " << nalu << std::endl;
+                assert(nalu);
+            }
+
+            previous_timestamp = timestamp;
+            ++frames;
+        }
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << "Exception: " << e.what() << "\n";
+    }
+
+    return 0;
+}
+
+
+int main(int argc, char* argv[])
+{
+    run_client_v2(argc, argv);
 }
---
+++ test/src/test_capture_data.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2016 Steinwurf ApS
+// All Rights Reserved
+//
+// THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF STEINWURF
+// The copyright notice above does not evidence any
+// actual or intended publication of such source code.
+
+#include <c4m/capture_data.hpp>
+
+#include <gtest/gtest.h>
+
+TEST(test_capture_data, single_nalu)
+{
+
+
+}
