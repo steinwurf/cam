d709ee954429cb95824d7347256667f04b8a9840
Morten V. Pedersen <morten@mortenvp.com>
2016-02-09 17:58:43 +0100

    Starting..

--- src/c4m/linux/layers.hpp
+++ src/c4m/linux/layers.hpp
@@ -15,10 +15,18 @@
 #include "../trace_layer.hpp"
 #include "trace_capture_layer.hpp"
 
+#include <linux/uvcvideo.h>
+#include <linux/usb/video.h>
+
 namespace c4m
 {
 namespace linux
 {
+
+    struct uvcx_video_config
+    {
+    };
+
 
     template<class Super>
     class set_key_frame_interval : public Super
@@ -27,51 +35,10 @@ namespace linux
 
         void start_streaming(std::error_code& error)
         {
-
+            // https://cgit.freedesktop.org/gstreamer/gst-plugins-bad/tree/sys/uvch264/gstuvch264_src.c
             assert(!error);
+            uvcx_video_config_probe_commit_t probe;
 
-            v4l2_ext_control extra_control;
-
-            memset(&extra_control, 0, sizeof(extra_control));
-            extra_control.id = V4L2_CID_MPEG_VIDEO_H264_I_PERIOD;
-            extra_control.size = 0;
-            extra_control.value = 10;
-
-            v4l2_ext_controls extra_controls;
-            memset(&extra_controls, 0, sizeof(extra_controls));
-
-            extra_controls.controls = &extra_control;
-            extra_controls.count = 1;
-            extra_controls.ctrl_class = V4L2_CTRL_CLASS_MPEG;
-
-            Super::retry_ioctl(VIDIOC_S_EXT_CTRLS, &extra_controls, error);
-
-            if (error)
-            {
-                std::cout << "Damn" << std::endl;
-                return;
-            }
-
-
-            Super::start_streaming(error);
-
-
-
-// if (0 < ret)
-// {
-//     printf("VIDIOC_S_EXT_CTRLS setting (%s)\n", strerror(errno));
-//     return -3;
-// }/*if*/
-
-
-// ret = ioctl(fd, VIDIOC_G_EXT_CTRLS, &extra_controls);
-//  if (0 < ret)
-// {
-//     printf("VIDIOC_G_EXT_CTRLS setting (%s)\n", strerror(errno));
-//     return -4;
-// }/*if*/
-
-// printf("extra_control.value = %d\n", extra_control.value );
 
         }
 
