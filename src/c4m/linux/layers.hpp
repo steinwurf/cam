bd8add6da3ac2fe04f297a51d052412132118f98
Morten V. Pedersen <morten@mortenvp.com>
2016-02-10 00:31:07 +0100

    Still figuring out stuff :)

--- src/c4m/linux/layers.hpp
+++ src/c4m/linux/layers.hpp
@@ -22,10 +22,82 @@ namespace c4m
 {
 namespace linux
 {
+    /// Development docs:
+    /// http://www.usb.org/developers/docs/devclass_docs/USB_Video_Class_1_5.zip
+    ///
+    /// The zip file contains a number of PDFs that we will reference in
+    /// the documentation below.
 
+    /// Constants needed
+    /// Defined in:
+    ///    UVC 1.5 Class specification.pdf
+    ///
+
+    /// UVCX Video Config struct is used for probing and to config a
+    /// specific configuration
+    ///
+    /// Defined in:
+    ///    USB Device Class Definition for Video Devices: H.264 Payload.
+    ///    Revision: 1.00
+    ///    Page 11.
+    ///    Link:
+    ///
     struct uvcx_video_config
     {
+        uint32_t m_frame_interval;
+        uint32_t m_bitrate;
+        uint16_t m_hints;
+        uint16_t m_configuration_index;
+        uint16_t m_width;
+        uint16_t m_height;
+        uint16_t m_slice_units;
+        uint16_t m_slice_mode;
+        uint16_t m_profile;
+        uint16_t m_i_frame_period;
+        uint16_t m_estimated_video_delay;
+        uint16_t m_estimated_max_config_delay;
+        uint8_t m_usage_type;
+        uint8_t m_rate_control_mode;
+        uint8_t m_temporal_scale_mode;
+        uint8_t m_spatial_scale_mode;
+        uint8_t m_snr_scale_mode;
+        uint8_t m_stream_mux_option;
+        uint8_t m_stream_format;
+        uint8_t m_entropy_cabac;
+        uint8_t m_timestamp;
+        uint8_t m_num_of_reorder_frames;
+        uint8_t m_preview_flipped;
+        uint8_t m_view;
+        uint8_t m_reserved1;
+        uint8_t m_reserved2;
+        uint8_t m_stream_id;
+        uint8_t m_spatial_layer_ratio;
+        uint16_t m_leaky_bucket_size;
     };
+
+    // Output operator for the uvcx_video_config
+    inline std::ostream&
+    operator<<(std::ostream& os, const uvcx_video_config& config)
+    {
+        os << "n4lu::annex_b_nalu: "
+           << "m_frame_interval = " << config.m_frame_interval << " "
+           << "m_bitrate = " << config.m_bitrate << " "
+           << "m_hints = " << config.m_hints << " "
+           << "m_configuration_index = " << config.m_configuration_index << " "
+           << "m_width = " << config.m_width << " "
+           << "m_height = " << config.m_height << " "
+           << "m_slice_units = " << config.m_slice_units << " "
+           << "m_slice_mode = " << config.m_slice_mode << " "
+           << "m_profile = " << config.m_profile << " "
+           << "m_i_frame_period = " << config.m_i_frame_period << " "
+           << "m_estimated_video_delay = "
+           << config.m_estimated_video_delay << " "
+           << "m_estimated_max_config_delay = "
+           << config.m_estimated_max_config_delay << " "
+           << "m_usage_type = " << config.m_usage_type << " ";
+
+        return os;
+    }
 
 
     template<class Super>
