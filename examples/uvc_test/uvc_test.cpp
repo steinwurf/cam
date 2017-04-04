2ce4cfeb20ea0d187ebd79b3acf2cd2986852cd0
Jeppe Pihl <jpihl08@gmail.com>
2017-04-04 09:59:14 +0200

    remove tcp sender + simplify uvc_test

--- examples/uvc_test/uvc_test.cpp
+++ examples/uvc_test/uvc_test.cpp
@@ -7,27 +7,16 @@
 
 #include <cam/linux/camera.hpp>
 
-int run_uvc(int argc, char* argv[])
-{
-    (void) argc;
-    (void) argv;
-
-    cam::linux::camera<cam::default_features> s;
-
-    s.try_open("/dev/video0");
-
-    std::cout << "w = " << s.width() << " "
-              << "h = " << s.height() << std::endl;
-
-    return 0;
-}
-
-
-int main(int argc, char* argv[])
+int main()
 {
     try
     {
-        run_uvc(argc, argv);
+        cam::linux::camera<cam::default_features> s;
+
+        s.try_open("/dev/video0");
+
+        std::cout << "w = " << s.width() << " "
+                  << "h = " << s.height() << std::endl;
     }
     catch (std::exception& e)
     {
